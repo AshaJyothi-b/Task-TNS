6bcb7ee776ff2356648c8e8b57a3c17195ac3071
user <user@LAPTOP-C0M4OJ7M>
2026-09-09 11:40:24 +0530

    Sum program

---
+++ Arrays/src/Arrays/Sum1.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class Sum1 {
+
+	public static void main(String[] args) {
+		        int[] arr = {2, 4, 6, 8, 10};
+		        int sum = 0;
+
+		        for(int i = 0; i < 5; i++) {
+		            sum = sum + arr[i];
+		        }
+
+		        System.out.println("Sum of all elements = " + sum);
+		    
+		
+
+	}
+
+}
