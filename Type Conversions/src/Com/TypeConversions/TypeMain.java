fd92e30a4668335a50b37e7d22553ae857d25086
user <user@LAPTOP-C0M4OJ7M>
2026-09-11 18:27:07 +0530

    TypeConversions

---
+++ Type Conversions/src/Com/TypeConversions/TypeMain.java
@@ -0,0 +1,12 @@
+package Com.TypeConversions;
+
+public class TypeMain {
+
+	public static void main(String[] args) {
+		TypeConversions td = new TypeConversions();
+		td.implicittype();
+		td.explicittype();
+
+	}
+
+}
