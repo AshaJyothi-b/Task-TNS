11436e5e1c707805aad2424c732df36af858f535
user <user@LAPTOP-C0M4OJ7M>
2026-09-12 19:20:20 +0530

    Access Modifiers

---
+++ AccessModifiers/src/Pack2/withoutChild.java
@@ -0,0 +1,15 @@
+package Pack2;
+
+import Pack1.sameClass;
+
+public class withoutChild {
+	
+	public static void main(String args[]) {
+	sameClass obj3 = new sameClass();
+	
+	//System.out.println(obj3.a);
+	//System.out.println(obj3.b);
+	//System.out.println(obj3.c);
+	System.out.println(obj3.d);
+}
+}
